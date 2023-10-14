2c0cbda2122b3599cd389f539638a6eecdc38c1b
Nikhil-Prabhakar <prabhakarnikhil2003@gmail.com>
2023-10-14 22:41:15 +0530

    Deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [currColor, setCurrColor] = useState("violet");
+  const [display, setDisplay] = useState("violet");
+  const updateColor = (col) => {
+    setCurrColor(col)
+    setDisplay(!display)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {display && (
+        <div>
+          <button
+            style={{ backgroundColor: "violet" }}
+            onClick={() => updateColor("violet")}
+          ></button>
+          <button
+            style={{ backgroundColor: "indigo" }}
+            onClick={() => updateColor("indigo")}
+          ></button>
+          <button
+            style={{ backgroundColor: "blue" }}
+            onClick={() => updateColor("blue")}
+          ></button>
+          <button
+            style={{ backgroundColor: "green" }}
+            onClick={() => updateColor("green")}
+          ></button>
+          <button
+            style={{ backgroundColor: "yellow" }}
+            onClick={() => updateColor("yellow")}
+          ></button>
+          <button
+            style={{ backgroundColor: "orange" }}
+            onClick={() => updateColor("orange")}
+          ></button>
+          <button
+            style={{ backgroundColor: "red" }}
+            onClick={() => updateColor("red")}
+          ></button>
+        </div>
+      )}
+      <div>
+        <button
+          style={{ backgroundColor: currColor, width: 200, borderRadius: 32 }}
+          onClick={() => {
+            setDisplay(!display);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Pick me
+        </button>
+      </div>
     </div>
   );
 }
